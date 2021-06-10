4db4594bed006f6feee1f7cc904957652f4409cd
unknown <enginoobz@gmail.com>
2021-06-10 18:08:18 +0200

    Implement SplashActivity

--- app/src/main/java/com/example/firebasetest/SplashActivity.kt
+++ app/src/main/java/com/example/firebasetest/SplashActivity.kt
@@ -1,10 +1,20 @@
 package com.example.firebasetest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (FirebaseAuth.getInstance().currentUser == null) {   // no already signed-in user
+            startActivity(Intent(this, AuthActivity::class.java))
+        } else {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        //close this SplashActivity
+        finish()
     }
 }
